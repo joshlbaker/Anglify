41103756ecd5ccea33d9f9b5265494d625d81cc8
Josh Baker <joshualbaker111@gmail.com>
2015-08-30 19:56:46 -0700

    fixed all api issues

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 angular.module('Anglify', [])
 
-	.controller('MainCtrl', ['$scope', '$http', function($scope, $http){
+	.controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
+		$scope.photos = [];
+
 		$scope.searchTag = function() {
-			console.log($scope.tag);
-			var url = 
-			$http.jsonp()
+			// console.log($scope.tag);
+								// https://api.soundcloud.com/tracks?q=3005&client_id=9116f0cf4ee47f19616a58e5f4640e52
+			var url =	'https://api.spotify.com/v1/search?q=' + $scope.tag + '&type=track&callback=JSON_CALLBACK';
+			$http.get(url)
+			// console.log($scope.tag);
+			.then(function (response) {
+				console.log(response.data.tracks.items[0].preview_url);
+				$scope.photos = response.data.tracks.items
+			});
 		};
 	}]);
+

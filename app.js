37217833f71c5037dc60bac1719ef221db20a248
Josh Baker <joshualbaker111@gmail.com>
2015-11-04 20:30:52 -0800

    design changes

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
 angular.module('Anglify', [])
 
 	.controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
-		$scope.photos = [];
+		$scope.tracks = [];
 
 		$scope.searchTag = function() {
-			// console.log($scope.tag);
-								// https://api.soundcloud.com/tracks?q=3005&client_id=9116f0cf4ee47f19616a58e5f4640e52
 			var url =	'https://api.spotify.com/v1/search?q=' + $scope.tag + '&type=track&callback=JSON_CALLBACK';
 			$http.get(url)
-			// console.log($scope.tag);
 			.then(function (response) {
-				console.log(response.data.tracks.items[0].external_urls.spotify);
-				$scope.photos = response.data.tracks.items
+				$scope.tracks = response.data.tracks.items
 			});
 		};
 	}]);
-
